563eca63a9b37f3067e97c88b160a7eca93d8dd6
raisultan <ki.xbozz@gmail.com>
2021-04-05 20:17:28 +0700

    config

---
+++ bot/app/config.py
@@ -0,0 +1,17 @@
+import secrets
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = '/api_v1'
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    X_API_TOKEN: str = 'no-secret-yet'
+
+    PROJECT_NAME: str = 'tg-bot-service'
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
