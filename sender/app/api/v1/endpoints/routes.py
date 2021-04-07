de0ef86cf08632f9850512e688834cb4c647650f
raisultan <ki.xbozz@gmail.com>
2021-04-08 03:25:47 +0700

    send_multiple route

--- sender/app/api/v1/endpoints/routes.py
+++ sender/app/api/v1/endpoints/routes.py
@@ -1,15 +1,24 @@
 from typing import Optional, NoReturn
+
 from fastapi import APIRouter, status, UploadFile, File
 
 from app import schemas
-from app.services import message_send, tg_document_create
+from app.services import message_send, message_send_multiple, tg_document_create
 
 router = APIRouter()
 
 
 @router.post('/send', status_code=status.HTTP_200_OK)
-async def send_plain_message(data: schemas.PlainMessageSend) -> Optional[NoReturn]:
-    await message_send(chat_id=data.chat_id, text=data.text)
+async def send_plain_message(message: schemas.PlainMessageSend) -> Optional[NoReturn]:
+    # TODO: consider handling exception on unknown PEER_ID
+    await message_send(chat_id=message.chat_id, text=message.text)
+    return
+
+
+@router.post('/send_multiple', status_code=status.HTTP_200_OK)
+async def send_multiple_messages(messages: list[schemas.PlainMessageSend]) -> Optional[NoReturn]:
+    # TODO: consider handling TG exceptions related to Timeout, TooMuchRequests, etc
+    await message_send_multiple(messages)
     return
 
 
@@ -19,5 +28,6 @@ async def send_plain_message(data: schemas.PlainMessageSend) -> Optional[NoRetur
         status_code=status.HTTP_201_CREATED,
 )
 async def create_document(document: UploadFile = File(...))-> dict[str, str]:
-    data = await tg_document_create(document)
-    return data
+    # TODO: consider handling TG exceptions related to Timeout, FileSize, etc
+    doc_created = await tg_document_create(document)
+    return doc_created
