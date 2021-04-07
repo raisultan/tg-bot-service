00ef64c62e95c498dd7317cd653b54e5f9a4c72b
raisultan <ki.xbozz@gmail.com>
2021-04-08 02:44:09 +0700

    create_document route

--- sender/app/api/v1/endpoints/routes.py
+++ sender/app/api/v1/endpoints/routes.py
@@ -1,17 +1,23 @@
 from typing import Optional, NoReturn
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, UploadFile, File
 
 from app import schemas
-from app.services import send_specific
+from app.services import message_send, tg_document_create
 
 router = APIRouter()
 
 
-@router.post("/send")
-async def send(
-        data: schemas.SendSpecific,
-        status_code=status.HTTP_200_OK,
-) -> Optional[NoReturn]:
-
-    await send_specific(data.chat_id, data.text)
+@router.post('/send', status_code=status.HTTP_200_OK)
+async def send_plain_message(data: schemas.PlainMessageSend) -> Optional[NoReturn]:
+    await message_send(chat_id=data.chat_id, text=data.text)
     return
+
+
+@router.post(
+        '/create_document',
+        response_model=schemas.DocumentCreate,
+        status_code=status.HTTP_201_CREATED,
+)
+async def create_document(document: UploadFile = File(...))-> dict[str, str]:
+    data = await tg_document_create(document)
+    return data
