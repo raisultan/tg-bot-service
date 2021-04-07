116b5ec39e849e947eba2d4bf563dbccf2254f23
raisultan <ki.xbozz@gmail.com>
2021-04-07 15:39:22 +0700

    single message send

--- sender/app/api/v1/endpoints/routes.py
+++ sender/app/api/v1/endpoints/routes.py
@@ -2,25 +2,16 @@ from typing import Optional, NoReturn
 from fastapi import APIRouter, status
 
 from app import schemas
-from app.services import init_bot
+from app.services import send_specific
 
 router = APIRouter()
 
 
-def send_specific(chat_id: str, text: str) -> Optional[NoReturn]:
-    bot = init_bot()
-    with bot:
-        bot.send_message(
-            chat_id=chat_id,
-            text=text,
-        )
-
-
 @router.post("/send")
-def send(
+async def send(
         data: schemas.SendSpecific,
         status_code=status.HTTP_200_OK,
 ) -> Optional[NoReturn]:
 
-    send_specific(data.chat_id, data.text)
+    await send_specific(data.chat_id, data.text)
     return
