67b6047e91841dfee0034d28f7d13d2a012b7b18
raisultan <ki.xbozz@gmail.com>
2021-04-07 15:30:38 +0700

    sync send

--- sender/app/api/v1/endpoints/routes.py
+++ sender/app/api/v1/endpoints/routes.py
@@ -1,6 +1,5 @@
 from typing import Optional, NoReturn
 from fastapi import APIRouter, status
-from pyrogram import Client
 
 from app import schemas
 from app.services import init_bot
@@ -8,26 +7,20 @@ from app.services import init_bot
 router = APIRouter()
 
 
-async def send_specific(bot: Client, chat_id: str, text: str) -> Optional[NoReturn]:
-    async with bot:
-        await bot.send_message(
+def send_specific(chat_id: str, text: str) -> Optional[NoReturn]:
+    bot = init_bot()
+    with bot:
+        bot.send_message(
             chat_id=chat_id,
             text=text,
         )
 
 
 @router.post("/send")
-async def send(
+def send(
         data: schemas.SendSpecific,
         status_code=status.HTTP_200_OK,
 ) -> Optional[NoReturn]:
 
-    bot = init_bot()
-    bot.run(
-        coroutine=await send_specific(
-            bot=bot,
-            chat_id=data.chat_id,
-            text=data.text,
-        ),
-    )
+    send_specific(data.chat_id, data.text)
     return
