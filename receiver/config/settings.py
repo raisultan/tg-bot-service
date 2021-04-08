6e5716565159f9413db5ca6839280059a13a831e
raisultan <ki.xbozz@gmail.com>
2021-04-08 22:04:26 +0700

    updated

--- receiver/config/settings.py
+++ receiver/config/settings.py
@@ -4,10 +4,13 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = 'tg-bot-service-receiver'
 
-    TG_BOT_TOKEN: str = 'your-bot-token'
-    TG_API_ID: str = 'your-bot-api-id'
-    TG_API_HASH: str = 'your-bot-api-hash'
-    TG_SESSION_NAME: str = 'tg_session'
+    TG_BOT_TOKEN: str = 'tg-bot-token'
+    TG_API_ID: str = 'tg-api-id'
+    TG_API_HASH: str = 'tg-api-hash'
+    TG_SESSION_NAME: str = 'tg_session_receiver'
+
+    GW_ROOT_URL: str = 'https://some.host'
+    GW_API_KEY: str = 'secret-api-key'
 
     class Config:
         case_sensitive = True
